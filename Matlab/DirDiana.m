c69a79e46c35a47088947f830ec98659b9a35d56
Laszlo Dobo <dobo.laszlou@gmail.com>
2012-10-11 22:44:37 +0200

    DirDiana fix 4 .DS_STORE, Diana gray

--- Matlab/DirDiana.m
+++ Matlab/DirDiana.m
@@ -1,10 +1,14 @@
 function [ ] = DirDiana( directory )
-
+    extensions = {'.jpg' '.jpeg'};
     files = dir( [directory '*' ]);
     for k = 1:numel(files)
         if ~files(k).isdir
-            img =  [directory files(k).name];
-            Diana(img);
+            [pathstr, name, ext] = fileparts(files(k).name);
+            lower(ext);
+            if eq(max(ismember(extensions,ext)),1)
+                img =  [directory files(k).name];
+                Diana(img);
+            end
         end    
     end
 
