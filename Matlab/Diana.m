456c81c22c7372d58cc6c649d7ee6c26887870d4
festo <gergely.munkacsy@gmail.com>
2012-10-11 21:51:48 +0200

    A muveletek lepesekre szedve. A kek reteg eltavolitva

--- Matlab/Diana.m
+++ Matlab/Diana.m
@@ -11,6 +11,11 @@ Rotated = false;
 % kep betoltese
 I = imread( filename );
 
+%kek reteg eltavolitasa
+%I(:,:,3) = 0;
+
+I_default = I;
+
 
 % Orientaltsag
 [height width d] = size(I);
@@ -22,8 +27,14 @@ end
 % structuring element
 se = strel('disk',5);
 
+%morfologiai muveletek
 I_opened = imopen(I,se);
 I_closed = imclose(I_opened,se);
-figure, imshow(I - I_closed)    
+
+%az eredeti kepbol kivonjuk a morfologiailag modositottat
+image = I_default - I_closed;
+
+%kep megjelenitese
+figure, imshow(image)    
 
 end
